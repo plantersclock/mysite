3c278c31718ecef390c7dec47a510bb1284faefd
Matthew Wright <mattman861@gmail.com>
2020-07-21 17:23:24 -0400

    formatted via black

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -4,12 +4,12 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_mnbfrb95w=+_1^z$1ei(2yxr8f5ixzjvi!gs%oo-nq)k7!&0$'
+SECRET_KEY = "_mnbfrb95w=+_1^z$1ei(2yxr8f5ixzjvi!gs%oo-nq)k7!&0$"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 try:
