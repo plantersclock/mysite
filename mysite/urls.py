d32b417c8255666cb53bb27728d3c4ea7bf0e0cc
Matthew Wright <mattman861@gmail.com>
2020-07-21 17:32:27 -0400

    added headless API V2 to wagtail

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,11 +8,14 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 
+from .api import api_router
+
 urlpatterns = [
     url(r"^django-admin/", admin.site.urls),
     url(r"^admin/", include(wagtailadmin_urls)),
     url(r"^documents/", include(wagtaildocs_urls)),
     url(r"^search/$", search_views.search, name="search"),
+    url(r"^api/v2/", api_router.urls),
 ]
 
 
